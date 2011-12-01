5cd5b3cb2dceb221d90f8204caffe9dad752694b
August Muench <august.fly@gmail.com>
2011-12-01 16:42:39 -0500

    first commit: art cluster data

---
+++ code/test_data.py
@@ -0,0 +1,12 @@
+"""
+    stub for: testing artificial cluster data
+"""
+
+def test_data_muench_mc_models():
+    return None
+
+def test_data_ic348():
+    return None
+
+def __main__()
+    return None
